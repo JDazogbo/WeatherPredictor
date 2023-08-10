0828583a57df59e5ae22e0a94cd49b69224c794e
JDazogbo <jdazo054@uottawa.ca>
2023-08-10 19:10:13 -0400

    Added further comments

--- WeatherSensors.X/BluetoothUART.h
+++ WeatherSensors.X/BluetoothUART.h
@@ -17,10 +17,10 @@
 
 //Parameter Variables
 #define BAUD_DELAY_TX 50 //This value in seconds might be lower than the theoretical value. Check with tests value to validate.
-#define BAUD_DELAY_RX BAUD_DELAY_TX-5
+#define BAUD_DELAY_RX BAUD_DELAY_TX-5//The Delay is considered to be slightly lower than the TX value. Play with these values for optimal performance
 #define BLUETOOTH_TX_GPIO 0//Pin that transmits info to the HC-05 module
 #define BLUETOOTH_RX_GPIO 1//Pin that receives signals from HC-05 module
-#define _XTAL_FREQ 4000000
+#define _XTAL_FREQ 4000000//Clock frequency of the uC
 
 //Usefull Macros
 #define setBit(variable, position) ((variable) |= 1 << (position))
@@ -33,7 +33,7 @@
 
 void reverseByte(unsigned char *pByte)
 {
-    //Code that reverses the character before sending it to the GPIO
+    //Code that reverses the character before sending it to the GPIO.
     unsigned char temp = 0;
     unsigned char temp2 = *pByte;
     for (unsigned char i = 8; i>0; i--)
@@ -48,26 +48,32 @@ void reverseByte(unsigned char *pByte)
 
 void readChar(unsigned char *pResult)
 {
-//    __delay_us(BAUD_DELAY/2); //We add a constant delay to take into account the time it takes to run the for loop instruction
+    //Add a delay for the first initial bit. This delay is useless for the pic10f200 as the pic is too slow
+    //__delay_us(BAUD_DELAY/2); //We add a constant delay to take into account the time it takes to run the for loop instruction
+    
+    //Reads a bit at a time and waits for the value.
     for (unsigned char i = 8; i>0; i--)
     {
         
         checkBit(GPIO, BLUETOOTH_RX_GPIO)? setBit(*pResult, i): clearBit(*pResult, i);
         __delay_us(BAUD_DELAY_RX); //NOTE: The delay here seems to be lower than the actual send rate delay. Play with values to get a correct output.
     }
-    //Code that reverses the character before sending it to the GPIO
+    //Reverses the byte when the value is known
     reverseByte(pResult);
 }
 
 void sendChar(unsigned char character)
 {
     //Version of the Code that is optimized for space. Use delay of 50 for baud rate 9600 on PIC10F200
-    //Start with the Start bit. This value indicates when the communication starts
-    //Code that reverses the character before sending it to the GPIO
-
+    
+    //Reverse the byte to begin with
     reverseByte(&character);
+    
+    //Start with the Start bit. This value indicates when the communication starts
     clearBit(GPIObits.GP0, BLUETOOTH_TX_GPIO); 
     __delay_us(BAUD_DELAY_TX);
+    
+    //Change the GPIO depending on the bit
     for (unsigned char i = 8; i>0; i--)
     {
         
@@ -75,57 +81,10 @@ void sendChar(unsigned char character)
         __delay_us(BAUD_DELAY_TX);
     }
     
+    //Set the end bit
     __delay_us(BAUD_DELAY_TX+30); //We add a constant delay to take into account the time it takes to run the for loop instruction
     setBit(GPIObits.GP0, BLUETOOTH_TX_GPIO);
     __delay_us(BAUD_DELAY_TX);
-
-    
-    
-    
-    
-//    //Version of the code that respects the baud rate more but looks uglier. Use this if higher baud rate is required. Use delay of 85 for baud 9600 on PIC10F200.
-//  
-//    //Code that reverses the character before sending it to the GPIO
-//    unsigned char temp = 0;
-//    unsigned char temp2 = character;
-//    for (unsigned char i = 8; i>0; i--)
-//    {
-//        temp = checkLSB(temp2)? temp*2+1: temp*2;
-//        temp2 = temp2>>1;
-//        
-//    }
-//    character = temp;
-    
-//    //Code that actually sends the signal
-//    
-//    GPIObits.GP0 = 0;
-//    __delay_us(BAUD_DELAY_TX);
-//    GPIObits.GP0 = checkLSB(character);
-//    character = character>>1;
-//    __delay_us(BAUD_DELAY_TX);
-//    GPIObits.GP0 = checkLSB(character);
-//    character = character>>1;
-//    __delay_us(BAUD_DELAY_TX);
-//    GPIObits.GP0 = checkLSB(character);
-//    character = character>>1;
-//    __delay_us(BAUD_DELAY_TX);
-//    GPIObits.GP0 = checkLSB(character);
-//    character = character>>1;
-//    __delay_us(BAUD_DELAY_TX);
-//    GPIObits.GP0 = checkLSB(character);
-//    character = character>>1;
-//    __delay_us(BAUD_DELAY_TX);
-//    GPIObits.GP0 = checkLSB(character);
-//    character = character>>1;
-//    __delay_us(BAUD_DELAY_TX);
-//    GPIObits.GP0 = checkLSB(character);
-//    character = character>>1;
-//    __delay_us(BAUD_DELAY_TX);
-//    GPIObits.GP0 = checkLSB(character);
-//    character = character>>1;
-//    __delay_us(BAUD_DELAY_TX);
-//    setBit(GPIObits.GP0, BLUETOOTH_TX_GPIO);
-//    __delay_us(BAUD_DELAY_TX);
     
     
 }
